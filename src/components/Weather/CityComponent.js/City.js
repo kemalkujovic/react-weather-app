a56b321e9705bef74f81f1e47175a22120ebce07
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-03-02 18:46:37 +0100

    stylingCity

--- src/components/Weather/CityComponent.js/City.js
+++ src/components/Weather/CityComponent.js/City.js
@@ -1,8 +1,15 @@
 import React from "react";
 import Card from "../../Card/Card";
+import logo from "../../../assets/icons/perfect-day.svg";
+import classes from "./City.module.css";
 const City = (props) => {
   return (
     <Card>
+      <div>
+        <h5>React Weather App</h5>
+        <img src={logo} className={classes.logo} />
+        <h5 className={classes["search-name"]}>Find Weather of your city</h5>
+      </div>
       <form>
         <input
           type="text"
@@ -10,6 +17,7 @@ const City = (props) => {
           name="city"
           placeholder="Search for city..."
         />
+        <button>Search</button>
       </form>
     </Card>
   );
