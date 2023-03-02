63673f83c794cde82229bffc154cd8425e29bced
Kemal Kujovic <kemalkujovic1111@gmail.com>
2023-03-02 22:45:27 +0100

    fetchingData

--- src/components/Weather/WeatherComponent/Weather.js
+++ src/components/Weather/WeatherComponent/Weather.js
@@ -2,13 +2,13 @@ import React from "react";
 import Card from "../../Card/Card";
 import sunset from "../../../assets/icons/temp.svg";
 import sunrise from "../../../assets/icons/temp.svg";
-import humidity from "../../../assets/icons/humidity.svg";
+import humidity1 from "../../../assets/icons/humidity.svg";
 import wind from "../../../assets/icons/wind.svg";
 import pressure from "../../../assets/icons/pressure.svg";
 import fada from "../../../assets/icons/sunny.svg";
 const Weather = (props) => {
-  console.log(props.data.weather[0].icon);
   const city = props.data.name;
+  const country = props.data.sys.country;
   const { description, id } = props.data.weather[0];
   const { feels_like, humidity, temp } = props.data.main;
   console.log(description, id, city, feels_like, humidity, temp);
@@ -28,6 +28,7 @@ const Weather = (props) => {
     "11d": "../../../assets/icons/storm.svg",
     "11n": "../../../assets/icons/storm.svg",
   };
+
   console.log(WeatherIcons[props.data?.weather[0].icon]);
   return (
     <Card>
@@ -38,6 +39,16 @@ const Weather = (props) => {
         <img
           src={`https://openweathermap.org/img/w/${props.data.weather[0].icon}.png`}
         />
+        <h1>{`${city}, ${country}`}</h1>
+        <div></div>
+        <img src={sunrise}></img>
+        <h1>{Math.round((props.data.main.feels_like - 273.15) * 10) / 10}</h1>
+        <img src={wind}></img>
+        <img src={pressure}></img>
+        <img src={humidity1}></img>
+        <h1>{props.data.main.humidity}</h1>
+        <h1>{props.data.main.pressure}</h1>
+        <h1>{props.data.wind.speed}</h1>
       </div>
     </Card>
   );
